b6155c1fd2d615f45a713e65b1dff67e78616225
PierreR <pierrer@pi3r.be>
2014-01-21 14:31:46 +0100

    Ignore possible bogus inputs on the client side

--- sub.hs
+++ sub.hs
@@ -1,13 +1,7 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes, OverloadedStrings #-}
 
 module Main
 where
-
-import Pipes
-import Pipes.Parse (Parser, isEndOfInput, evalStateT)
-import qualified Pipes.Parse as PP
-import qualified Pipes.ZMQ4 as PZ
-import qualified System.ZMQ4 as Z
 
 import Control.Concurrent(threadDelay)
 import Control.Concurrent.Async
@@ -16,9 +10,17 @@ import Control.Applicative((<$>), (<*>))
 import Control.Lens(zoom)
 import qualified Control.Foldl as L
 
-import Data.ByteString.Char8 (pack, unpack)
+import qualified Data.ByteString.Char8 as BC
+
+import Pipes
+import Pipes.Parse (Parser, isEndOfInput, evalStateT)
+import qualified Pipes.Parse as PP
+import qualified Pipes.ZMQ4 as PZ
+
 import Text.Printf
 import System.Random (randomRIO)
+import qualified System.ZMQ4 as Z
+
 
 pubServerThread :: Z.Sender t => Z.Socket t -> IO r
 pubServerThread s = forever $ do
@@ -26,15 +28,8 @@ pubServerThread s = forever $ do
     zipcode <- randomRIO (10000::Int, 11000)
     temperature <- randomRIO (-10::Int, 35)
     humidity <- randomRIO (10::Int, 60)
-    let update = pack $ unwords [show zipcode, show temperature, show humidity]
+    let update = BC.pack $ unwords [show zipcode, show temperature, show humidity]
     Z.send s [] update
-
--- | This function will be part of foldl later on
---   @fold (premap f folder) list == fold folder (map f list)@
-premap :: (a -> b) -> L.Fold b r -> L.Fold a r
-premap f (L.Fold step begin done) = L.Fold step' begin done
-  where
-    step' x = step x . f
 
 average :: L.Fold Int Int
 average = div <$> L.sum <*> L.length
@@ -51,12 +46,12 @@ main = do
             async $ pubServerThread pubSocket
 
             Z.connect subSocket "inproc://pubserver"
-            Z.subscribe subSocket (pack "10001")
+            Z.subscribe subSocket "10001"
 
             evalStateT reportParser (processedData subSocket)
     where
 
-        reportParser :: Parser (Int, Int, Int) IO ()
+        reportParser :: Parser (Int, Int) IO ()
         reportParser = loop
             where
                 loop = do
@@ -65,21 +60,23 @@ main = do
                     eof <- isEndOfInput
                     unless eof loop
 
-        averages :: L.Fold (Int, Int, Int) (Int, Int)
+        averages :: L.Fold (Int, Int) (Int, Int)
         averages =
-            let avgTemp :: L.Fold (Int, Int, Int) Int
-                avgTemp = premap (\(_, t, _) -> t) average
+            let avgTemp :: L.Fold (Int, Int) Int
+                avgTemp = L.premap fst average
 
-                avgHumidity :: L.Fold (Int, Int, Int) Int
-                avgHumidity = premap (\(_, _, h) -> h) average
+                avgHumidity :: L.Fold (Int, Int) Int
+                avgHumidity = L.premap snd average
 
             in  (,) <$> avgTemp <*> avgHumidity
 
-        processedData :: Z.Socket Z.Sub -> Producer (Int, Int, Int) IO ()
+        processedData :: Z.Socket Z.Sub -> Producer (Int, Int) IO ()
         processedData subSocket = for (PZ.fromZMQ subSocket) $ \bs -> do
-            let [zipcode, temperature, humidity] = map read $ words (unpack bs)
-            liftIO $ printf "At NY City (%d), temperature of %d and humidity %d\n"  zipcode temperature humidity
-            yield (zipcode, temperature, humidity)
+            case map BC.readInt (BC.words bs) of
+                [_, Just (temp, _), Just(hum, _)] -> do
+                    liftIO $ printf "At NY City, temperature of %d and humidity %d\n"  temp hum
+                    yield (temp, hum)
+                [_, _, _]                         -> return()
 
 
 
